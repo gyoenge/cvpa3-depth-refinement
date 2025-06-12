15a5c905158a43ce168c0f79d11a412052ca7d7f
gyoenge <gyoeng94@gmail.com>
2025-06-12 13:23:21 +0000

    fix main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from SDR.baseline.train import train as train_baseline
 from SDR.baseline.inference import inference as inference_baseline
 from SDR.archboost.train import train as train_archboost
 from SDR.archboost.inference import inference as inference_archboost 
+from SDR.databoost.train import train as train_databoost 
 from SDR.test import *
 
 
@@ -10,16 +11,16 @@ Baseline
 """
 
 # train_baseline(
-#     data_dir='./data/aug0.002', #'./data/augmentation/',
+#     data_dir='./data/ori_train/',
 #     save_dir='./output/baseline/',
 #     holefilling_kernel_size=7,
-#     holefilling_iter=50, 
+#     holefilling_iter=30, 
 #     batch_size=8,
-#     epoch=100, 
-#     learning_rate=1e-4,
+#     epoch=10000, 
+#     learning_rate=5e-3,
 #     weight_decay=1e-1,
 #     alpha=1.0,
-#     beta=0.7,
+#     beta=0.2,
 # )
 
 # test_d2n(
@@ -33,7 +34,7 @@ Baseline
 #     save_dir='./output/baseline/inference/',
 #     model_path='./result_sota/baseline/weight.pth',
 #     holefilling_kernel_size=7,
-#     holefilling_iter=50, 
+#     holefilling_iter=30, 
 #     batch_size=1, 
 # )
 
@@ -69,10 +70,52 @@ ArchBoost
 #     is_avgpool_d2n=True, 
 # )
 
+# inference_archboost(
+#     data_dir='./data/ori_test/',
+#     save_dir='./output/archboost/inference/',
+#     model_path='./result_sota/archboost/weight.pth',
+#     holefilling_kernel_size=13,
+#     holefilling_iter=30, 
+#     batch_size=1, 
+#     # ArchBoost setting
+#     smooth_initial=True,
+#     learnable_d2n=True,
+# )
+
+
+"""
+DataBoost
+"""
+
+# train_databoost(
+#     data_dir='./data/aug0.002/',   # './data/ori_test/',
+#     save_dir='./output/databoost/',
+#     #### 
+#     model_path='./result_sota/archboost/weight.pth',
+#     has_gt=False, 
+#     #### 
+#     holefilling_kernel_size=13,
+#     holefilling_iter=30, 
+#     batch_size=8, 
+#     epoch=5000, 
+#     learning_rate=1e-3, 
+#     weight_decay=1e-1,
+#     lr_scheduling=True, 
+#     alpha=1.0,
+#     beta=0.2,
+#     ## ArchBoost setting
+#     smooth_initial=True,
+#     learnable_d2n=True,
+#     auxiliary_loss=True,
+#     lambda_aux=0.2, 
+#     ## Save setting
+#     save_checkpoint=True, 
+# )
+
 inference_archboost(
     data_dir='./data/ori_test/',
-    save_dir='./output/archboost/inference/',
-    model_path='./result_sota/archboost/weight.pth',
+    save_dir='./output/databoost/inference/',
+    model_path='./result_sota/databoost/weight.pth',
     holefilling_kernel_size=13,
     holefilling_iter=30, 
     batch_size=1, 
